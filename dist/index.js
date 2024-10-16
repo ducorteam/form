bb9277a74d429fe9a696d760f730475646d0d73e
Jubayed Bhuiyan <70032786+jubayed@users.noreply.github.com>
2024-10-16 18:49:32 +0600

    init

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+"use client";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.useForm = void 0;
+const useForm = () => {
+};
+exports.useForm = useForm;
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+"use client";
+
+const useForm = () => {
+
+}
+
+export {useForm}
